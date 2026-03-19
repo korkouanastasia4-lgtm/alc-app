b19431a4964224823c6880af872af1e5c435781b
korkouanastasia4-lgtm <korkouanastasia4@gmail.com>
2026-03-19 20:05:44 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,4 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+  <rect width="100" height="100" rx="22" fill="#7a1f2a"/>
+  <text x="50" y="58" text-anchor="middle" font-size="34" font-family="Arial, sans-serif" fill="white">ALC</text>
+</svg>
